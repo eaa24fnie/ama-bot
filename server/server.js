340fa60e4658515fbbc0b357a91fa365bfd59d94
eaa24fnie <eaa24fnie@students.eaaa.dk>
2026-09-25 08:08:58 +0200

    Nu virker den også på Mac iguess

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 import express from "express";
+import cors from "cors";
 import messagesRouter from "./routes/messages.js";
 import answersRouter from "./routes/answers.js";
-import cors from "cors";
 
 const app = express();
 const port = 3000;
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.use("/messages", messagesRouter);
 app.use("/answers", answersRouter);
